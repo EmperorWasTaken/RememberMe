bcaef469e6fb6e8bb76bf3c3dfa38da984bd984c
Kai Kåsa <ktkaas18@uia.no>
2021-03-26 16:05:58 +0100

    Made design changes to task cards

--- app/src/main/java/com/example/rememberme/data/TasksRecyclerAdapter.kt
+++ app/src/main/java/com/example/rememberme/data/TasksRecyclerAdapter.kt
@@ -10,7 +10,6 @@ class TasksRecyclerAdapter(private var tasks:List<Task>, private val onTaskClick
     class ViewHolder(val binding:TaskLayoutBinding):RecyclerView.ViewHolder(binding.root){
         fun bind(task: Task, onTaskClicked:(Task) -> Unit){
             binding.titleTask.text = task.title
-            binding.contentTask.text = task.content
 
             binding.taskCard.setOnClickListener {
                 onTaskClicked(task)
