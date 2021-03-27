84890742b3bd9d15ddb0e8726bee09ef2e4c0504
Kai Kåsa <ktkaas18@uia.no>
2021-03-27 10:43:45 +0100

    Added and changed for Lists and Tasks

---
+++ app/src/main/java/com/example/rememberme/data/ListDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rememberme.data
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_details)
+    }
+}
