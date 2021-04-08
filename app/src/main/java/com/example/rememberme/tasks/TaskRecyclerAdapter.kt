1e730de8a9b2d4187539bc866fd3611d1f8a10fe
Kai Kåsa <ktkaas18@uia.no>
2021-04-08 18:51:29 +0200

    Bug fix with task delete

--- app/src/main/java/com/example/rememberme/tasks/TaskRecyclerAdapter.kt
+++ app/src/main/java/com/example/rememberme/tasks/TaskRecyclerAdapter.kt
@@ -2,6 +2,7 @@ package com.example.rememberme.tasks
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
@@ -10,6 +11,7 @@ import com.example.rememberme.TaskListHolder
 import com.example.rememberme.data.Task
 import com.example.rememberme.data.TaskList
 import com.example.rememberme.databinding.ActivityTaskDetailsBinding
+import com.example.rememberme.lists.TaskListDetailsActivity
 import com.example.rememberme.lists.TaskListsDepositoryManager
 import com.example.rememberme.lists.TaskListsRecyclerAdapter
 
@@ -26,18 +28,13 @@ class TaskRecyclerAdapter (private var tasks:List<Task>, private var contexts: C
             binding.deleteTaskButton.setOnClickListener{
                 TaskListsDepositoryManager.instance.removeTaskInList(TaskListHolder.ClickedList, task, position)
                 Toast.makeText(contexts, "Task deleted", Toast.LENGTH_LONG).show()
+                contexts.startActivity(Intent(contexts, TaskListDetailsActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
                 (contexts as Activity).finish()
             }
 
-            /*binding.taskCheckBox.setOnClickListener{
+            binding.taskCheckBox.setOnClickListener{
 
-                val progress = !task.onChecked
-                binding.taskCheckBox.isChecked = progress
-
-                TaskListsDepositoryManager.instance.updateTaskInListProgress(task, progress)
-
-
-            }*/
+            }
 
         }
     }
